ad52ec81264d9a5287be8643049eeb8216254066
Daniel Castaño <daniel.zapata.682@gmail.com>
2024-01-06 12:23:23 -0500

    Add: Generate Report button

--- Views/front_alquiler/src/views/Reports.jsx
+++ Views/front_alquiler/src/views/Reports.jsx
@@ -1,5 +1,7 @@
 import ButtonCataComponente from "../components/provider/Button/Button"
 import { Button } from "react-bootstrap";
+import * as XLSX from 'xlsx'
+// import ReportPreview from "./report/ReportPreview";
 // import XLSXPopulate from "xlsx-populate";
 
 const subsidies_balances = async () => {
@@ -41,16 +43,55 @@ const getReportData = async () => {
   }
 }
 
-// const createReport = async (data) => {
-//   const workbook = await XLSXPopulate.fromBlankAsync();
-//   const sheet = workbook.sheet(0);
-// }
+function formatearFecha(fecha) {
+  const fechaFormateada = new Date(fecha).toLocaleDateString('es-CO', { timeZone: 'UTC' })
+  return `${fechaFormateada} `;
+}
+
+const createReport = async (data) => {
+  const libro = XLSX.utils.book_new();
+  const hojaDia = XLSX.utils.json_to_sheet(data.dia.map(row => ({
+    ...row,
+    FechadPago: formatearFecha(row.FechadPago),
+    createdAt: formatearFecha(row.createdAt),
+  })));
+
+  // Agregar el resultado de SALDO_TOTAL a la hoja de ABONOS
+  const hojaAbonos = XLSX.utils.json_to_sheet([
+    ...data.abonos.map(row => ({
+      ...row,
+      FechadPago: formatearFecha(row.FechadPago),
+    })),
+  ]);
+
+  // Crea una hoja de Excel con los resultados de 'SALDOS'
+  const hojaSaldos = XLSX.utils.json_to_sheet(data.saldos.map(row => ({
+    ...row,
+    FechadPago: formatearFecha(row.FechadPago),
+  })));
+
+  // Crea una hoja de Excel con los resultados de 'GASTOS'
+  const hojaGastos = XLSX.utils.json_to_sheet(data.gastos.map(row => ({
+    ...row,
+    createdAt: formatearFecha(row.createdAt),
+  })));
+
+  XLSX.utils.book_append_sheet(libro, hojaDia, 'DIA');
+  XLSX.utils.book_append_sheet(libro, hojaAbonos, 'ABONOS');
+  XLSX.utils.book_append_sheet(libro, hojaGastos, 'GASTOS');
+  XLSX.utils.book_append_sheet(libro, hojaSaldos, 'SALDOS');
+
+  XLSX.writeFile(libro, `reporte.xlsx`);
+}
+
+
 
 const handleReport = async () => {
   const data = await getReportData()
-  // await createReport(data)
-  console.log(data)
+  createReport(data)
 }
+
+
 
 const Reports = () => {
   return (<>
